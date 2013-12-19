cc8e79621f30a3e89eb675c549803daeb20683a1
jlas <juan.lasheras@gmail.com>
2013-12-19 01:40:17 -0200

    slightly faster solution to problem 32

--- p32.py
+++ p32.py
@@ -9,17 +9,17 @@ http://projecteuler.net/problem=32
 
 import time
 
-pan = '123456789'
 prods = set()
 
 def pandigital():
     for i in xrange(1, 10000):
+        stri = str(i)
         for j in xrange(1, 10000):
             prod = i*j
-            _pan = ''.join(sorted(str(i) + str(j) + str(prod)))
+            _pan = stri + str(j) + str(prod)
             if len(_pan) > 9:  # bail since results just getting bigger now
                 break
-            if pan == _pan and not prod in prods:
+            if '123456789' == ''.join(sorted(_pan)) and not prod in prods:
                 prods.add(prod)
     print sum(prods)
 
